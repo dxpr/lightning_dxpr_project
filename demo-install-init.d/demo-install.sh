aac5c22a7c17e4c80bd3ba1bacf53153da741bb3
mhmelshaaer <m.elshaaer@dxpr.com>
2021-03-15 13:50:38 +0200

    create a demo-install-init.d dir that will run as an entry point script for the php docker service - adding env variables to make the demo install script configurable

--- demo-install-init.d/demo-install.sh
+++ demo-install-init.d/demo-install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# Environment variables
-if [ ! -f ".env" ]; then
-    echo "\e[38;5;178m[WARNING]\e[0m env file does not exist."
-    return
-fi
-
-. ./.env
+set -eo pipefail
 
 # DXPR access token
 if [ -z "$DXPR_ACCESS_TOKEN" ]
 then
-      echo "\e[38;5;178m[WARNING]\e[0m DXPR_ACCESS_TOKEN is empty"
-      return
+      echo "DXPR_ACCESS_TOKEN is empty"
+      exit 1;
 fi
 
 # Configure the dxpr access token
@@ -27,7 +21,7 @@ cp docroot/sites/default/default.settings.php docroot/sites/default/settings.php
 mkdir -p docroot/sites/default/files && chmod -R 777 docroot/sites/default/files
 
 # Installing DXPR QA demo website
-drush site-install lightning_dxpr lightning_dxpr_demo_select.demo_select=dxpr_qa_demo --db-url=mysql://drupal:drupal@mariadb:3306/drupal --account-pass=123 -y -v
+drush site-install lightning_dxpr lightning_dxpr_demo_select.demo_select=$DXPR_DEMO --db-url=mysql://$DB_USER:$DB_PASSWORD@mariadb:3306/$DB_NAME --account-pass=$DXPR_ADMIN_PASSWORD -y -v
 
 # Allow accessing website assets
 chmod -R 777 docroot/sites/default/files
